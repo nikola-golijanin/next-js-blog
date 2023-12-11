7cad880888b44c2130fcf0ada5b9da6bff750bcd
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-12-11 11:34:21 +0100

    all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,37 @@
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    excerpt: "Next js is production ready react framework...",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    excerpt: "Next js is production ready react framework...",
+  },
+
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    excerpt: "Next js is production ready react framework...",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    excerpt: "Next js is production ready react framework...",
+  },
+];
+
 export default function AllPostsPage() {
-  return <div>AllPostsPage</div>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
